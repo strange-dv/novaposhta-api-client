ebfe13b2f4a84866d903b5c3d3152bbfbf3781e6
strange-dv <andrii.dovzhenko.dv@gmail.com>
2022-01-11 14:45:39 +0200

    made ready for pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
 from distutils.core import setup
 
+
 setup(
-    name='novaposhta-api-client',
-    version='0.0.9',
-    packages=['novaposhta'],
-    url='https://github.com/semolex/novaposhta-api-client',
-    install_requires=[
+  name = 'novaposhta',
+  packages = ['novaposhta'],
+  version = '0.0.9',
+  license='MIT',
+  description = 'Python client for Nova Poshta company\'s API.',
+  author = 'andrii dovzhenko',
+  author_email = 'andrii.dovzhenko.dv@gmail.com',
+  url = 'https://github.com/strange-dv/novaposhta-api-client',
+  download_url = 'https://github.com/strange-dv/novaposhta-api-client/archive/refs/tags/v0.0.9.tar.gz',
+  keywords = ['nova', 'poshta'],
+  install_requires=[
         'requests',
     ],
-    license='MIT',
-    author='semolex (Oleksii Semeshchuk)',
-    author_email='semolex@live.com',
-    description='Python client for Nova Poshta company\'s API.'
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.10',
+  ],
 )
